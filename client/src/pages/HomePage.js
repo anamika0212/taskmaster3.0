ef6a2448e766377b1c6f6c46bfeb6b6b34dce34c
unknown <jordanabravo2009@yahoo.com>
2021-11-22 20:11:56 -0800

    added css, login, signup etc.

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Navigate } from "react-router-dom";
+
+import NavTabs from "../components/MUI/NavTabs";
+import { AuthContext } from "../context/AuthContext";
+
+export default class HomePage extends Component {
+  state = {
+    redirect: false
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        redirect: true
+      });
+    }, 2000);
+  }
+
+  render() {
+
+    return (
+      <AuthContext>
+        {({ user }) => {
+          if (!user) {
+            if (this.state.redirect) {
+              return <Navigate to="/login" />;
+            }
+          }
+          return (
+            <>
+              <h2 className="text-center">Task Master App</h2>
+              <NavTabs />
+            </>
+          );
+        }}
+      </AuthContext>
+    );
+  }
+}
